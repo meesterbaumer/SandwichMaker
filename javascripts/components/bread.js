590a5ee46f567122d3c82def689615d25afed3a4
Michael Baumer <meesterbaumer@mac.com>
2019-09-28 10:53:19 -0500

    Linked Bread option to printtodom

---
+++ javascripts/components/bread.js
@@ -0,0 +1,8 @@
+import utilities from '../helpers/utilities.js';
+
+const printBreadOptions = () => {
+    const domString = 'bread';
+    utilities.printToDom('bread', domString);
+};
+
+export default { printBreadOptions };
