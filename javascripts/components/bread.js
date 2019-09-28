9ba80b2e4505dfe85da8378fa13b56b7eff30293
Michael Baumer <meesterbaumer@mac.com>
2019-09-28 11:29:22 -0500

    Added checkboxes

--- javascripts/components/bread.js
+++ javascripts/components/bread.js
@@ -1,7 +1,37 @@
 import utilities from '../helpers/utilities.js';
 
+const breads = 
+[
+    {
+        id: "bread1", 
+        name: "White", 
+        price: 50 
+    },
+    {
+        id: "bread2", 
+        name: "Wheat", 
+        price: 70 
+    },
+    {
+        id: "bread3", 
+        name: "Sourdough", 
+        price: 30 
+    },
+
+];
+
 const printBreadOptions = () => {
-    const domString = 'bread';
+    let domString = '';
+    for(let i=0; i < breads.length; i++) {
+        domString +=
+        `
+        <div class="form-group form-check">
+        <input type="checkbox" class="form-check-input" id="${breads[i].id}>
+        <label class="form-check-label" for=${breads[i].id}>${breads[i].name}</label>
+        </div>
+        `;
+    }
+    
     utilities.printToDom('bread', domString);
 };
 
